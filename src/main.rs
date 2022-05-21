6b33c04dd3ca1527f3ecd3570dbe6c4623ec55e3
adrianohellermylla <adrianohellermylla@gmail.com>
2022-05-21 15:00:02 -0300

    chore: Initial commit in repo

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use std::collections;
+use std::collections::HashMap;
+
+fn main() {
+    let name = "Straits".to_string();
+    // Full substring
+    let sub_string = &name[..];
+    // Iterating over Splitted Scalar values
+    for letter in sub_string.chars() {
+        println!("{}",letter);
+    }
+    // Iterating over real bytes
+    for letter in sub_string.bytes() {
+        println!("{}",letter);
+    }
+    // This is the inner storing of a String
+    let name:Vec<u8> = vec![83,116,114,97,105,116,115];
+    // Associate and merge bytes as Utf-8 String
+    let s = String::from_utf8(name).expect("Cannot convert into string.");
+
+    println!("{}",s);
+
+    let name = String::from("محمد");
+
+    let num_bytes = name.bytes();
+
+    let num_chars = name.chars();
+
+    for i in num_bytes {
+        println!("{}",i);
+    }
+
+    for c in num_chars {
+        println!("{}",c);
+    }
+
+    let maome_in_bytes: Vec<u8> = vec![217,133,216,173,217,133,216,175];
+
+    let mount_string = String::from_utf8(maome_in_bytes).unwrap();
+
+    println!("{}",mount_string);
+
+    // Hash Maps
+
+    let mut team_scores = collections::HashMap::new();
+
+    team_scores.insert(String::from("Yankees"),27);
+
+    team_scores.insert(String::from("Jets"),21);
+
+    println!("{:#?}",team_scores);
+
+    let companies: Vec<String> = vec![String::from("Tesla"),String::from("Red Hat"),String::from("Mavics")];
+
+    let valuation_in_billions: Vec<i32> = vec![200,8,2];
+
+    // Joining and collecting iterated values from different vectors together
+    let mut associated_valuation: HashMap<_,_> = companies.into_iter().zip(valuation_in_billions.into_iter()).collect();
+
+    println!("{:#?}",associated_valuation);
+
+}
